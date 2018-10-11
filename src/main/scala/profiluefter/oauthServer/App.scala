ab304afae5f05105b6ba14a39ae2cda54dbd1788
Fabian <fabian.paul.gurtner@gmail.com>
2018-10-11 21:13:54 +0200

    Register Applications and Users move from id column to the automatically created rowid

--- src/main/scala/profiluefter/oauthServer/App.scala
+++ src/main/scala/profiluefter/oauthServer/App.scala
@@ -10,15 +10,16 @@ object App {
   def main(args: Array[String]): Unit = {
     val serverSocket = new ServerSocket(1337)
 
+    while (true) {
+      val clientSocket = serverSocket.accept()
 
-    val clientSocket = serverSocket.accept()
+      val writer = new PrintStream(clientSocket.getOutputStream)
+      val reader = new BufferedSource(clientSocket.getInputStream).getLines()
 
-    val writer = new PrintStream(clientSocket.getOutputStream)
-    val reader = new BufferedSource(clientSocket.getInputStream).getLines()
+      handle(writer, reader)
 
-    handle(writer, reader)
-
-    clientSocket.close()
+      clientSocket.close()
+    }
   }
 
 
@@ -29,6 +30,19 @@ object App {
         val password = reader next
 
         writer.print(Database.login(username, password))
+      case "register_app" =>
+        val website = reader next
+        val description = reader next
+        val redirectUrl = reader next
+
+        writer.print(Database.registerApp(website, description, redirectUrl))
+      case "register_user" =>
+        val username = reader next
+        val email = reader next
+        val password = reader next
+
+        writer.print(Database.registerUser(username, email, password))
+      case _ => writer.print("DU FLOSCHN KONNST KOANE BEFEHLE HEANEHMA HEAST?")
     }
   }
 }
--- src/main/scala/profiluefter/oauthServer/Database.scala
+++ src/main/scala/profiluefter/oauthServer/Database.scala
@@ -5,17 +5,54 @@ import java.sql.DriverManager
 import org.mindrot.jbcrypt.BCrypt
 
 object Database {
+  def registerUser(username: String, email: String, password: String): Int = {
+    val url = "jdbc:sqlite:db.sqlite"
+    val connection = DriverManager.getConnection(url)
+
+    val statement = connection.prepareStatement(
+      "INSERT INTO users (username, passwordHash, email) VALUES (?,?,?)"
+    )
+    statement.setString(1, username)
+    statement.setString(2, BCrypt.hashpw(password, BCrypt.gensalt()))
+    statement.setString(3, email)
+    statement.executeUpdate()
+  }
+
+  def registerApp(website: String, description: String, redirectUrl: String): Int = {
+    val url = "jdbc:sqlite:db.sqlite"
+    val connection = DriverManager.getConnection(url)
+
+    val statement = connection.prepareStatement(
+      "INSERT INTO applications (website, description, redirect_url) VALUES (?,?,?)"
+    )
+    statement.setString(1, website)
+    statement.setString(2, description)
+    statement.setString(3, redirectUrl)
+    statement.executeUpdate()
+  }
+
   def login(username: String, password: String): Int = {
     val url = "jdbc:sqlite:db.sqlite"
     val connection = DriverManager.getConnection(url)
-    val statement = connection.prepareStatement("SELECT id,passwordHash FROM users where username=?")
-    statement.setString(1,username)
+
+    val statement = connection.prepareStatement("SELECT rowid,passwordHash FROM users where username=?")
+    statement.setString(1, username)
     statement.execute()
+
     val resultSet = statement.getResultSet
-    val hash = resultSet.getString("passwordHash")
-    if(BCrypt.checkpw(password,hash))
-      resultSet.getInt("id")
-    else
-      -1
+    if (resultSet.next()) {
+      val hash = resultSet.getString("passwordHash")
+      if (BCrypt.checkpw(password, hash)) {
+        val id = resultSet.getInt("rowid")
+        connection.close()
+        id
+      } else {
+        connection.close()
+        -1
+      }
+    } else {
+      connection.close()
+      -2
+    }
   }
 }
